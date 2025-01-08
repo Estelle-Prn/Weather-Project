5fa84bd7692df3abf29efc89a926c5b8e433bba2
Estelle-Prn <peroni.estelle@gmail.com>
2025-01-08 08:16:38 +0100

    added id in html and wrote some js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function IDK(event) {
+  event.preventDefault();
+  let ResultCityTap = document.querySelector("#SearchText");
+  let ShowCity = document.querySelector("#CityName");
+  ShowCity.innerHTML = ResultCityTap.value;
+}
 
+let ResultSearchBlock = document.querySelector("#search-block");
+ResultSearchBlock.addEventListener("submit", IDK);
