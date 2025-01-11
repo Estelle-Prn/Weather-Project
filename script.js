d5ad4943132a745278790aa00575a1f138557284
Estelle-Prn <peroni.estelle@gmail.com>
2025-01-11 11:01:55 +0100

    Added time and day in real time

--- script.js
+++ script.js
@@ -8,12 +8,34 @@ function CorrespondingTemperature(response) {
   let DayTime = document.querySelector("#DayAndTime");
   let date = new Date(response.data.time * 1000);
 
-  DayTime.innerHTML = `${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+  DayTime.innerHTML = ActualDate(date);
   windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
   humidity.innerHTML = `${response.data.temperature.humidity}%`;
   description.innerHTML = response.data.condition.description;
   ShowCity.innerHTML = response.data.city;
   UpdateTemperature.innerHTML = Math.round(temperature);
+}
+function ActualDate(date) {
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hour}:${minutes}`;
 }
 
 function APIwork(city) {
